0c415304c9a7d1ef7c80ff48ed182d15a1f79d3c
kociokwik <78303091+kociokwik@users.noreply.github.com>
2023-07-20 14:41:29 +0200

    Added PrestaShop 1.7.8.8 classes and controllers

---
+++ classes/assets/StylesheetManager.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+* 2023 Sebastian Jóźwiak
+*
+* NOTICE OF LICENSE
+*
+* This source file is subject to the Open Software License (OSL 3.0)
+* that is bundled with this package in the file LICENSE.txt.
+* It is also available through the world-wide-web at this URL:
+* http://opensource.org/licenses/osl-3.0.php
+* If you did not receive a copy of the license and are unable to
+* obtain it through the world-wide-web, please send an email
+* to license@prestashop.com, so we can send you a copy immediately.
+*
+* DISCLAIMER
+*
+* Please read the included README for instructions.
+*
+*  @author Sebastian Jóźwiak <kontakt@scriptd.com>
+*  @copyright  2023 Sebastian Jóźwiak, SCRIPTD
+*  @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+*/
+
+class StylesheetManager extends StylesheetManagerCore
+{
+
+}
